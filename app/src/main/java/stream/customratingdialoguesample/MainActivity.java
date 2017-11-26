70ebd18d2a70c9cdc1ab571d96df73ce6e7ccabe
Ray Li <ray.li.verified@gmail.com>
2017-11-26 17:59:44 -0600

    Import Rating Dialogue
    *Import Custom Rating Dialogue code.

---
+++ app/src/main/java/stream/customratingdialoguesample/MainActivity.java
@@ -0,0 +1,50 @@
+package stream.customratingdialoguesample;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import stream.customratingdialogue.RatingDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RatingDialog mRatingDialog;
+
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getApplication().getApplicationContext();
+        setContentView(R.layout.activity_main);
+
+        mRatingDialog = new RatingDialog(MainActivity.this);
+        mRatingDialog.setRatingDialogListener(new RatingDialog.RatingDialogInterFace() {
+            @Override
+            public void onDismiss() {
+                Log.d("RATELISTERNER","onDismiss");
+            }
+
+            @Override
+            public void onSubmit(float rating) {
+                Log.d("RATELISTERNER","onSubmit "+ rating);
+            }
+
+            @Override
+            public void onRatingChanged(float rating) {
+                Log.d("RATELISTERNER","onRatingChanged "+ rating);
+            }
+        });
+
+        TextView textView = findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRatingDialog.showDialog();
+            }
+        });
+    }
+}
