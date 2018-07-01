9ff162bb669cfee2a73e55c2a44a6e032bbad700
Ray Li <ray.li.verified@gmail.com>
2018-07-01 13:24:26 -0500

    Update AndroidX
    *Update to AndroidX.
    *Update Gradle to v3.2.0-beta02.
    *TargetSDK v28.

--- customratingdialogue/src/main/java/stream/customratingdialogue/RotationRatingBar.java
+++ customratingdialogue/src/main/java/stream/customratingdialogue/RotationRatingBar.java
@@ -2,7 +2,7 @@ package stream.customratingdialogue;
 
 import android.content.Context;
 import android.os.Handler;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
--- customratingdialogue/src/main/java/stream/customratingdialogue/SimpleRatingBar.java
+++ customratingdialogue/src/main/java/stream/customratingdialogue/SimpleRatingBar.java
@@ -1,7 +1,7 @@
 package stream.customratingdialogue;
 
 import android.graphics.drawable.Drawable;
-import android.support.annotation.DrawableRes;
+import androidx.annotation.DrawableRes;
 
 interface SimpleRatingBar {
 
